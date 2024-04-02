f88c94b0cebaf53afba58209288bfa2807aa374e
Fettuccinifelix <nicholassan67@gmail.com>
2024-04-01 23:13:15 -0700

    trying to clean up mask

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 import cv2 as cv
 import numpy as np
-from PIL import Image
+
 
 
 
@@ -20,10 +20,20 @@ while 1:
     upper_blue = np.array([130, 255, 255])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+    bluemask = cv.inRange(hsv, lower_blue, upper_blue)
+    bluemask_channels = cv.split(bluemask)
+
+
+    #Correct for nonuniform illumination
+    greymask = bluemask_channels[0]
+    se = cv.getStructuringElement(cv.MORPH_RECT, (8, 8))
+    bg = cv.morphologyEx(greymask, cv.MORPH_DILATE, se)
+    out_grey = cv.divide(greymask, bg, scale=255)
+    out_binary = cv.threshold(out_grey, 0, 255, cv.THRESH_OTSU)[1]
+
 
     # find contours
-    contours = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[-2]
+    contours = cv.findContours(greymask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)[-2]
     for c in contours:
         # calculate moments for each contour
         M = cv.moments(c)
@@ -34,15 +44,13 @@ while 1:
             cY = int(M["m01"] / M["m00"])
         else:
             cX, cY = 0, 0
-        cv.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
-        cv.putText(frame, "centroid", (cX - 25, cY - 25), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     #for c in contours:
     #    x, y, w, h = cv.boundingRect(c)
     #    cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     #    cv.rectangle(mask, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    '''
+
     hull_list = []
     for i in range(len(contours)):
         hull = cv.convexHull(contours[i])
@@ -50,11 +58,11 @@ while 1:
 
     for h in hull_list:
         cv.drawContours(frame, [h], -1, (0, 255, 0), 3)
-    '''
+
 
 
     cv.imshow('frame', frame)
-    cv.imshow('mask', mask)
+    cv.imshow('mask', out_binary)
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
